94bc3a65a4e0dd4e2558854d0b6565f104b3e558
V <V@MacBook-Pro-de-V.local>
2022-01-16 14:04:39 +0100

    exercise 8 half done

---
+++ Exercise 8/Index_8.js
@@ -0,0 +1,18 @@
+function checkNumberOfA(){
+    let phrase = document.getElementById('input').value;
+    let phraseToString = phrase.toString();
+    let findCharacter = phraseToString.match(/[a]/gi).length;
+    
+
+    if(findCharacter == false){
+        resultContainer.innerHTML = "<h3>There are no a's on the phrase</h3>";
+    }
+    
+    else{
+        resultContainer.innerHTML = "<h3>There are " + findCharacter + " a's on the phrase</h3>";
+    }
+
+    return
+}
+
+// not finished
