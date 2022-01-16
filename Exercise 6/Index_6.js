c855a4414f8b570fd61019147f5d48feee470d63
V <V@MacBook-Pro-de-V.local>
2022-01-16 12:32:21 +0100

    exercise 6 solved

---
+++ Exercise 6/Index_6.js
@@ -0,0 +1,28 @@
+    function biggestNumber(){
+        let numb1 = Number.parseInt(document.getElementById('input1').value)
+        let numb2 = Number.parseInt(document.getElementById('input2').value)
+        let numb3 = Number.parseInt(document.getElementById('input3').value)
+
+        let result = Math.max(numb1,numb2,numb3)
+
+
+        resultContainer.innerHTML = "<h3> The biggest number is " + result + "!</h3>";
+}
+
+
+        //-------------------------------- ANOTHER SOLUTION -------------------------------//
+
+    /* function biggestNumber(){
+        let numb1 = Number.parseInt(document.getElementById('input1').value)
+        let numb2 = Number.parseInt(document.getElementById('input2').value)
+        let numb3 = Number.parseInt(document.getElementById('input3').value)
+
+        if ((numb1) > (numb2) && (numb1) > (numb3)){
+            resultContainer.innerHTML = "<h3> The biggest number is " + numb1 + "!</h3>";
+        }
+        else if ((numb2) > (numb1) && (numb2) > (numb3)){
+           resultContainer.innerHTML = "<h3> The biggest number is " + numb2 + "!</h3>";
+        }
+        else
+          resultContainer.innerHTML = "<h3> The biggest number is " + numb3 + "!</h3>";
+    } */
